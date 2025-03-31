d475e1fbd2152c4dfcc554d771bcb6bd17003af0
Yuanze Lin <yuanze.lin@cs.ox.ac.uk>
2025-03-31 23:29:18 +0100

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -4,8 +4,9 @@ MODELDIR=ckpts/$model_name
 python predict.py \
   --prompt "Generate an image of a fluffy orange cat lounging on a windowsill, \
 with sunlight streaming through the glass and casting soft shadows to create a cozy atmosphere. Next, \
-change the cat’s color to white. In the following step, produce a high-resolution 3D model based on the modified image." \
+would it be possible to change the cat's color to white? This change will make it more eye-catching. \
+In the following step, produce a high-resolution 3D model based on the modified image. \
+At the next point, please show a video of a cat and a dog running on a playground." \
   --model-path $MODELDIR \
   --temperature 0 \
   --conv-mode v0
-
